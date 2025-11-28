7db8b7cbb7d55d113ef0c3ae08343310f7df0470
Rein McKelway <rein.mckelway@mail.utoronto.ca>
2025-11-28 12:31:50 -0500

    wow

--- src/main/java/regex/Main.java
+++ src/main/java/regex/Main.java
@@ -1,5 +1,6 @@
 package regex;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -24,11 +25,6 @@ public class Main {
         System.out.println(checkForDoubles(userInput));
     }
 
-    // Method 1 for checking if a string matches a regex: using Pattern.matches
-    // TODO: Modify this code to check if the given string is non-empty, contains at least
-    //       one lower case letter, at least one upper case letter, and at least one digit. If the
-    //       string has all of these properties, the method should return true. If it is missing one
-    //       or more properties, it should return false.
 
     /**
      * Returns whether a given string is non-empty, contains one lower case letter,
@@ -38,16 +34,24 @@ public class Main {
      * @return whether the string satisfies the password requirements
      */
     public static boolean checkForPassword(String str, int minLength) {
-        final boolean propertyOne = Pattern.matches("REPLACE WITH CORRECT REGEX", str);
-        // as needed, modify this code.
-        return propertyOne;
+       if (str == null) {
+           return false;
+       }
+       else if (str.length() < minLength) {
+           return false;
+       }
+       else if (!Pattern.matches(".*[A-Z].*", str)) {
+           return false;
+       }
+       else if (!Pattern.matches(".*[a-z].*", str)) {
+           return false;
+       }
+       else if (!Pattern.matches(".*[0-9].*", str)) {
+           return false;
+       }
+       return true;
     }
 
-    // Method 2 for checking if a string conforms to a regex: using Matcher.find
-    // TODO: Modify this method to return a list of all email addresses contained in the
-    //       input string that end with "@mail.utoronto.ca" or "@utoronto.ca" with at least one
-    //       character before the "@" symbol. The email addresses should be in the order they
-    //       appear in the string.
 
     /**
      * Returns a list of email addresses that occur in a given string.
@@ -55,7 +59,10 @@ public class Main {
      * @return a list containing the email addresses in the string.
      */
     public static List<String> extractEmails(String str) {
-        final Pattern pattern = Pattern.compile("REPLACE WITH CORRECT REGEX");
+        if (str == null) {
+            return new  ArrayList<>();
+        }
+        final Pattern pattern = Pattern.compile("[a-zA-Z0-9]+(@mail.utoronto.ca|@utoronto.ca)");
         final Matcher matcher = pattern.matcher(str);
         final List<String> result = new ArrayList<>();
         while (matcher.find()) {
@@ -66,16 +73,29 @@ public class Main {
 
     // Method 3 for checking if a string conforms to a regex: using String.matches
 
-    // TODO: Modify this method to check whether or not the string contains the same capital letter
-    //       twice. For example "Amazing Apple" contains "A" twice. If the string does repeat the same
-    //       capital letter twice, the method should return true. Otherwise it should return false.
-
     /**
      * Checks whether a given string contains the same capital letter twice.
      * @param str the string to look for doubles in
      * @return whether str contains the same capital letter twice.
      */
     public static boolean checkForDoubles(String str) {
-        return str.matches("replace with correct regex");
+        if  (str == null) {
+            return false;
+        }
+
+        ArrayList<Character> capitals = new ArrayList<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (Character.isUpperCase(c)) {
+                if (capitals.contains(c)) {
+                    return true;
+                }
+                else  {
+                    capitals.add(c);
+                }
+            }
+        }
+        return false;
+
     }
 }
